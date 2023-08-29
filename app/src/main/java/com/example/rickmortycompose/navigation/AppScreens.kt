7a0cdc27ef5a7eeedf66d8719f5429d20f3a38e1
MisaelMota <87102180+MisaelMota@users.noreply.github.com>
2023-08-29 14:52:22 -0400

    Problems with the navigation, when press de navigation item

--- app/src/main/java/com/example/rickmortycompose/navigation/AppScreens.kt
+++ app/src/main/java/com/example/rickmortycompose/navigation/AppScreens.kt
@@ -5,7 +5,6 @@ import com.example.rickmortycompose.R
 sealed class AppScreens(val route:String){
 
     object MainScreen: AppScreens("CharactersScreen")
-
     object EpisodesScreen:AppScreens("EpisodesScreen")
     object UserScreen:AppScreens("UserScreen")
     object DetailCharacterScreen: AppScreens("DetailCharacterScreen/{id}")
